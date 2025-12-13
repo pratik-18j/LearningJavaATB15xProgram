dee3bf3c69cb996dcaaece25e60ad45900858222
pratik-18j <jpartik1891@gmail.com>
2025-12-13 19:07:14 +0530

    Update Main.java with new print statement

--- src/ex_01_Java_Basics/Main.java
+++ src/ex_01_Java_Basics/Main.java
@@ -2,7 +2,6 @@ package ex_01_Java_Basics;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello JDK-21!");
+        System.out.println("Hello JDK-21!!!!");
     }
 }
-git
